600343765c47c50cffd236da27c182e70670f283
Peter Spiess-Knafl <dev@spiessknafl.at>
2020-03-17 12:07:43 +0100

    Improve test coverage

--- main_test.go
+++ main_test.go
@@ -22,6 +22,35 @@ func TestHealth(t *testing.T) {
 	assert.Equal(t, "<html><body><img width=\"500\" src=\"https://spiessknafl.at/helth.png\"/></body></html>", string(greeting))
 }
 
+func emptyPage(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<html></html>"))
+}
+
+func TestErrors(t *testing.T) {
+
+	ecdcURL := ecdcExporter.url
+	ministryURL := ministryExporter.url
+
+	mockServer := httptest.NewServer(http.HandlerFunc(emptyPage))
+	defer mockServer.Close()
+	ecdcExporter.url = mockServer.URL
+	ministryExporter.url = mockServer.URL
+
+	ts := httptest.NewServer(http.HandlerFunc(handleHealth))
+
+	defer ts.Close()
+	response, err := ts.Client().Get(ts.URL)
+	assert.Nil(t, err)
+	assert.Equal(t, 500, response.StatusCode)
+	greeting, err := ioutil.ReadAll(response.Body)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "<html><body><img width=\"500\" src=\"https://spiessknafl.at/fine.jpg\"/><pre>World stats are failing\nCould not find \"Bestätigte Fälle\"\nMissing ministry stats\nCould not find metric cov19_confirmed / ()\nCould not find metric cov19_tests / ()\nCould not find metric cov19_healed / ()\n</pre></body></html>", string(greeting))
+
+	ecdcExporter.url = ecdcURL
+	ministryExporter.url = ministryURL
+}
+
 func TestMetrics(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(handleMetrics))
 	defer ts.Close()
